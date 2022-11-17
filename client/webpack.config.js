6a63bdfa153c48a8acbeac3efaf2f57c1a4947f6
ssavane26 <savaneprod@gmail.com>
2022-11-17 11:28:54 -0500

    Working on webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,7 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // Plug Ins for HTML v v v v
     plugins: [
       
     ],
