b8a37b5ea8af77d72b9e1bfec1b86881710c919e
ssavane26 <savaneprod@gmail.com>
2022-11-17 15:49:47 -0500

    Pushing webpack TODOs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,12 +19,55 @@ module.exports = () => {
     },
     // Plug Ins for HTML v v v v
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      // Custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // Creates Manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'PWA text editor application',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
     module: {
+      // CSS
       rules: [
-        
+         {
+          test: /.css$/i,
+          use: ['style-loader', 'css-loader'],
+         },
+         {
+          test: /.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+         },
       ],
     },
   };
